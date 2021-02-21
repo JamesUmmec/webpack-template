385757152c862d6795fe968c69331a8a96ada186
JamesUmmex <dreamingboat@outlook.com>
2021-02-21 16:44:09 +0800

    better structure for change

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import * as $ from "jquery"
 // those are just templates.
 // delete those and add your code.
 
-onload = () => { setInterval(() => {
-	$("#now").text((new Date()).toLocaleString())
-}, 10) }
+onload = () => {
+	setInterval(() => {
+		$("#now").text((new Date()).toLocaleString())
+	}, 10)
+}
